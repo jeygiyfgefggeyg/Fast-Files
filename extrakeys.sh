a054812c9fd4001701f13229a87bc14e214b3ba9
jeygiyfgefggeyg <92650029+jeygiyfgefggeyg@users.noreply.github.com>
2021-10-20 16:57:52 -0400

    Add files via upload

---
+++ extrakeys.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1 && logout
+
